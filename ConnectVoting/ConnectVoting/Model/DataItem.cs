57d5da55cc394e636ac6fd4ced56899c7543b466
Wm. Barrett Simms <barrett@wbsimms.com>
2016-10-07 17:38:58 -0500

    Initial Commit

---
+++ ConnectVoting/ConnectVoting/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace ConnectVoting.Model
+{
+	public class DataItem
+	{
+		public string Title
+		{
+			get;
+			private set;
+		}
+
+		public DataItem(string title)
+		{
+			Title = title;
+		}
+	}
+}
